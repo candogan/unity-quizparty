a93dd6b107d126dd83cf94318b2919348d0333c8
Max <max.rosemeier@gmail.com>
2022-12-21 15:51:43 +0100

    clean project for usage of gitignore file

--- Spielfeld/Assets/Greek Monster Pack/Common/Scripts/Profiles/EyeMaterialProfile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EyeMaterialProfile", menuName = "Greek Monster Pack/Eye Material Profile", order = 7)]
-public class EyeMaterialProfile : ScriptableObject
-{
-    public List<SimpleMaterialInformation> usableMaterials = new List<SimpleMaterialInformation>();
-}
