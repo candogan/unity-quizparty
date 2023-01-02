14b29132426573feb49c715fa40d711fa130f3de
candogan <dogan_can@mail.de>
2023-01-02 15:34:35 +0100

    Update GameField class

--- Spielfeld/Assets/Scripts/GamePlayContent/GameField/GameField.cs
+++ Spielfeld/Assets/Scripts/GamePlayContent/GameField/GameField.cs
@@ -6,47 +6,22 @@ using System;
 
 [Serializable]
 public class GameField {
-    int posX;
-    int posY;
-    int posZ;
+    Vector3 position;
     int type;
     string nextFieldMove;
 
-    public GameField (int posX, int posY, int posZ, int type, string nextFieldMove) {
-        this.posX = posX;
-        this.posY = posY;
-        this.posZ = posZ;
+    public GameField (Vector3 position, int type, string nextFieldMove) {
+        this.position = position;
         this.type = type;
         this.nextFieldMove = nextFieldMove;
     }
 
-    public int getPosX(){
-        return posX;
+    public Vector3 getPosition(){
+        return position;
     }
 
-    public int getPosY(){
-        return posY;
-    }
-
-    public int getPosZ(){
-        return posZ;
-    }
-
-    public void setPosX(int newPosX){
-        posX = newPosX;
-    }
-
-    public void setPosY(int newPosY){
-        posY = newPosY;
-    }
-
-    public void setPosZ(int newPosZ){
-        posZ = newPosZ;
-    }
-
-    public int[] getPos(){
-        int[] posArray = {posX, posY, posZ};
-        return posArray;
+    public void setPosition(Vector3 newPosition){
+        posX = newPosition;
     }
 
     public void setNextFieldMove(string newNextFieldMove){
