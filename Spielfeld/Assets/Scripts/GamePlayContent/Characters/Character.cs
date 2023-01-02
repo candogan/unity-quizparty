3de83b0ac405ec58ebcfc6696867798454a2a978
candogan <dogan_can@mail.de>
2023-01-02 15:24:55 +0100

    Copied new map into QuizParty scene

---
+++ Spielfeld/Assets/Scripts/GamePlayContent/Characters/Character.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Vector3 initPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
