5f84689d7f35e328dec86f5d57c78095b5cac616
candogan <dogan_can@mail.de>
2023-01-02 16:05:12 +0100

    New Managers added

---
+++ Spielfeld/Assets/Scripts/GamePlayContent/GameField/NextFieldDirectionEnum.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NextFieldDirectionEnum
+{
+    public static string RIGHT = "RIGHT";
+    public static string LEFT = "LEFT";
+    public static string STRAIGHT = "STRAIGHT";
+}
