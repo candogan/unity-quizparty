6af94e09b6bf413cca0d4cecf35155d7f8cb0d29
candogan <dogan_can@mail.de>
2023-01-02 17:04:24 +0100

    Update Spielfeld

--- Spielfeld/Assets/Scripts/GamePlayContent/Characters/Character.cs
+++ Spielfeld/Assets/Scripts/GamePlayContent/Characters/Character.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     Vector3 initPosition;
+    Vector3 currentLocation;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,14 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentLocation = transform.position;
+        if (currentLocation.x >= initPosition.x - 7) {
+                Animator.SetTrigger();
+                Vector3 destiny = new Vector3(0, 0, 2) * Time.deltaTime;
+                transform.Translate(destiny);
+        } else {
+            Vector3 speed = new Vector3(0, 0, 0) * Time.deltaTime;
+            transform.Translate(speed);
+        }
     }
 }
