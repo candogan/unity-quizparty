0229e39b7e4cc536aed67d7c1505ea0e8c4e5e76
niklaswbr <niklas_weber@unitybox.de>
2023-03-09 16:42:54 +0100

    Main Menu Info Box

--- QuizParty/Assets/Scripts/Menu/DownUpManager.cs
+++ QuizParty/Assets/Scripts/Menu/DownUpManager.cs
@@ -5,9 +5,33 @@ using SimpleFileBrowser;
 using System.IO.Compression;
 using System;
 using System.Diagnostics;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DownUpManager : MonoBehaviour
 {
+
+	public GameObject dialog;
+	public TMP_Text text;
+
+	public void ShowDialog(int i, String path){
+		dialog.SetActive(true);
+		if (i == 0){
+			text.GetComponent <TMP_Text> ().text = "Die Frageliste wurde auf Ihrem Desktop unter " + path + " gespeichert";
+		}else{
+			text.GetComponent <TMP_Text> ().text = "Die Frageliste wurde erfolgreich importiert";
+		}
+	}
+
+	public void DeactivateUI(){
+		dialog.SetActive(false);
+		text.GetComponent <TMP_Text> ().text = "";
+	}
+
+	public void confirmButton(){
+		dialog.SetActive(false);
+		text.GetComponent <TMP_Text> ().text = "";
+	}
 
 
 	public void GetPath()
@@ -42,15 +66,18 @@ public class DownUpManager : MonoBehaviour
 			UnpackResources();
 
 			File.Delete(Application.dataPath + "/Resources/" +  "RessourceOrdner.zip");
+
+			ShowDialog(1, "");
+
 		}
 	}
 
-    public static void SaveFile()
+    public void SaveFile()
     {
 		var timestamp = DateTime.Now.ToFileTime();
         string startPath = Application.dataPath + "/Resources/";
         string zipPath =  Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Resources" + timestamp + ".zip";
-
+		ShowDialog(0, zipPath);
         ZipFile.CreateFromDirectory(startPath, zipPath);
     }
 
@@ -64,7 +91,7 @@ public class DownUpManager : MonoBehaviour
 		System.IO.DirectoryInfo di = new DirectoryInfo(Application.dataPath + "/Resources/");
 		foreach (FileInfo file in di.GetFiles())
 		{
-			if (file.Name == "unity default resources" || file.Name == "unity_builtin_extra" || file.Name == "saveGameQuestions.json")
+			if (file.Name == "unity default resources" || file.Name == "unity_builtin_extra")
 			{
 				return;
 			} 
